909aa8e6103a16bdee461f18d59d544bbc7e8615
Eunju Chong <eun01300714@gmail.com>
2019-04-19 15:26:16 +0900

    NCP_lec3

---
+++ NCP_lec3/NCP_lec3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NCP_lec3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int output = int.MinValue;
+            Console.WriteLine(output);
+            Console.WriteLine(-1 * output);
+            Console.WriteLine(-2147483648);
+            Console.WriteLine((long)-1 * -2147483648);
+
+            long output2 = long.MinValue;
+            Console.WriteLine(output2);
+            Console.WriteLine(-1 * output2);
+            Console.WriteLine(-9223372036854775808);
+            Console.WriteLine((float) -1 * -9223372036854775808);
+            Console.WriteLine((double)-1 * -9223372036854775808);
+        }
+    }
+}
